32a5445da9d03a24945ab107b5686437029b9633
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-27 17:42:46 -0500

    init files

---
+++ Seed_bamazon.sql
@@ -0,0 +1,13 @@
+DROP DATABASE IF EXISTS bamazon;
+
+CREATE DATABASE bamazon;
+
+USE bamazon;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    product_name VARCHAR(60) NOT NULL,
+    department_name VARCHAR(60),
+    price DECIMAL(10, 2) NOT NULL,
+    stock_quantity INT NOT NULL
+);
