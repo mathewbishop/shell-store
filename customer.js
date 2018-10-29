3f966232c24b4668606360cb30f8a06305b75fc8
Mathew Bishop <39043451+MattyyB@users.noreply.github.com>
2018-10-29 13:51:35 -0500

    feature; displays products on start, then prompts questions

--- customer.js
+++ customer.js
@@ -1,8 +1,13 @@
+// Require packages
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const fs = require("fs");
+const Table = require("cli-table");
 
-const DB_Connection = mysql.createConnection({
+//===========================================================
+// Connection info for the DB
+//===========================================================
+const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
@@ -10,6 +15,46 @@ const DB_Connection = mysql.createConnection({
     database: "shell_storeDB"
 });
 
-const makeConnection = () => {
-    DB_Connection.connect()
+
+
+//===========================================================
+// Display all products
+//===========================================================
+const displayProducts = () => {
+    connection.query(
+    "SELECT * FROM products",
+        (err, res) => {
+            if (err) throw err;
+            console.log(res);
+        }
+    )
+    connection.end();
 }
+
+//===========================================================
+// Initial prompt 
+//===========================================================
+const initialPrompt = () => {
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'productID',
+            message: 'Please enter the ID # of the product you want to purchase: '
+        },
+        {
+            type: 'input',
+            name: 'quantityDesired',
+            message: 'Please enter the quantity you wish to purchase: '
+        }
+    ]).then(answers => {
+        let productSelection = answers.productID;
+        let quantity = answers.quantityDesired;
+        console.log(productSelection);
+        
+    })
+} 
+
+
+
+displayProducts();
+setTimeout(initialPrompt, 1000);
